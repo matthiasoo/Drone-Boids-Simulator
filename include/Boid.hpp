cc64751b7be8bce8a98289b77102d12cfbaf623f
matthiasoo <makky.x.55@gmail.com>
2026-05-07 00:58:08 +0200

    Radius controls for boid rules

--- include/Boid.hpp
+++ include/Boid.hpp
@@ -9,15 +9,15 @@ private:
     float maxSpeed;
     float maxForce;
 
-    Vector2D separate(const std::vector<Boid>& boids);
-    Vector2D align(const std::vector<Boid>& boids);
-    Vector2D cohere(const std::vector<Boid>& boids);
+    Vector2D separate(const std::vector<Boid>& boids, float radius);
+    Vector2D align(const std::vector<Boid>& boids, float radius);
+    Vector2D cohere(const std::vector<Boid>& boids, float radius);
 
 public:
     Boid(float x, float y);
     void applyForce(const Vector2D& force);
     void update();
-    void flock(const std::vector<Boid>& boids, float wSep, float wAli, float wCoh);
+    void flock(const std::vector<Boid>& boids, float wSep, float wAli, float wCoh, float rSep, float rAli, float rCoh);
     void edges(float width, float height);
     Vector2D getPosition() const;
     Vector2D getVelocity() const;
--- src/Swarm.cpp
+++ src/Swarm.cpp
@@ -6,9 +6,9 @@ void Swarm::addBoid(float x, float y) {
     boids.emplace_back(x, y);
 }
 
-void Swarm::updateAll(float wSep, float wAli, float wCoh) {
+void Swarm::updateAll(float wSep, float wAli, float wCoh, float rSep, float rAli, float rCoh) {
     for (auto& boid : boids) {
-        boid.flock(boids, wSep, wAli, wCoh);
+        boid.flock(boids, wSep, wAli, wCoh, rSep, rAli, rCoh);
         boid.update();
         boid.edges(1280.0f, 720.0f);
     }
